f563794c028c1090b23ac68c0848cf2b5296c595
andreytsimbalov <andrey.tsimbalov@mail.ru>
2019-09-28 18:37:48 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+'''
+Paint, написанный на python
+'''
+
+
+from tkinter import *
+import setUI as s
+
+class Paint(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        s.setUI(self)
+        self.brush_size = 10
+        self.brush_color = "black"
+
+    def draw(self, event):
+        self.canv.create_oval(event.x - self.brush_size,
+                              event.y - self.brush_size,
+                              event.x + self.brush_size,
+                              event.y + self.brush_size,
+                              fill=self.brush_color, outline=self.brush_color)
+
+    def set_color(self, new_color): # изменение цвета кисти
+        self.brush_color = new_color
+
+    def set_brush_size(self, new_size): # изменение размера кисти
+        self.brush_size = new_size
+
+
+
+def main():
+    root = Tk()
+    root.geometry("640x480+100+100")
+    app = Paint(root)
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
